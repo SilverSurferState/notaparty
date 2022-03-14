e28e9e236c8f0fbed8a85c8ca013e7e16cc83988
beto <beto@asuvorm.be>
2022-03-14 19:24:20 +0100

    first commit and creation

---
+++ src/main/java/com/beto/notaparty/NotapartyApplication.java
@@ -0,0 +1,13 @@
+package com.beto.notaparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotapartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotapartyApplication.class, args);
+    }
+
+}
